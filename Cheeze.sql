e33e9b6aa7c0516dfdeb8d3efedbb7ff882671d1
Roshni <62755400+belhyto@users.noreply.github.com>
2022-12-28 11:51:25 +0530

    Create Cheeze.sql

---
+++ Cheeze.sql
@@ -0,0 +1,31 @@
+
+create database inventory_db;
+ use inventory_db;
+ 
+ create table customer
+     ( cust_id int(11) PRIMARY KEY Auto_increment,
+     name varchar(120),
+     address varchar(120),
+    contact int(11));
+    
+    --when ID is auto increment then neglect from insertion
+    
+    insert into customer ( name, address,contact)
+    -> values ('Hiko','1290 tokyo','123456789'),
+    -> ('Jemo','1340 tokyo','12342389'),
+    -> ('oNa','4340 tokyo','123232449');
+    
+    --Change data value in table
+    
+     Update customer set name='Hena' where cust_id=1;
+     
+    --Delete a row of data in table
+     Delete from customer where cust_id=3;
+     
+     --Display all table
+     select * from customer;
+     
+     --Display all data of specified row from table
+     select * from customer where cust_id=2;
+     
+     
